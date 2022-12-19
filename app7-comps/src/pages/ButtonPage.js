703bf2b072a293222bd42684bd009956c5569454
chaithalbe <chet071000@users.noreply.github.com>
2022-12-19 09:54:23 +0530

    refactor ButtonPage

--- app7-comps/src/pages/ButtonPage.js
+++ app7-comps/src/pages/ButtonPage.js
@@ -6,25 +6,51 @@ export default function ButtonPage() {
 	return (
 		<div>
 			<div>
-				<Button primary rounded className={"mb-2"}>
+				<Button primary className='mb-1'>
 					<GoBell />
 					Primary
 				</Button>
 			</div>
 			<div>
-				<Button secondary outline>
+				<Button secondary className='mb-1'>
 					<GoCloudDownload />
 					Secondary
 				</Button>
 			</div>
 			<div>
-				<Button warning>Warning</Button>
+				<Button warning className='mb-1'>
+					Warning
+				</Button>
 			</div>
 			<div>
-				<Button success>Success</Button>
+				<Button success className='mb-1'>
+					Success
+				</Button>
 			</div>
 			<div>
-				<Button danger>Danger</Button>
+				<Button danger className='mb-1'>
+					Danger
+				</Button>
+			</div>
+			<div>
+				<Button rounded className='mb-1'>
+					Rounded
+				</Button>
+			</div>
+			<div>
+				<Button outline className='mb-1'>
+					Outline
+				</Button>
+			</div>
+			<div>
+				<Button className='mb-1'>
+					<GoBell /> With Icon
+				</Button>
+			</div>
+			<div>
+				<Button className='mb-1'>
+					With Icon <GoCloudDownload />
+				</Button>
 			</div>
 		</div>
 	);
